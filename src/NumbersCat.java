df49146fa4c56f8ae48eed1955227eaf745fe0e1
PepGuiem <82808082+PepGuiem@users.noreply.github.com>
2023-01-25 15:37:38 +0100

    Add files via upload

--- src/NumbersCat.java
+++ src/NumbersCat.java
@@ -1,5 +1,4 @@
 public class NumbersCat {
-
     public static String say(long n) {
         if (n < 0) {
             return "Menys " + say(-n).toLowerCase();
@@ -48,35 +47,54 @@ public class NumbersCat {
                 return "Noranta";
         }
 
-        int unidad = 0;
+        int unitat = 0;
         int decena = 0;
+        int divisor = 0;
+        String xifra = "";
+        String xifres = "";
+
         if (n > 20 && n < 100) {
-            unidad = (int) n % 10;
+            unitat = (int) n % 10;
             decena = 10 * ((int) n / 10);
             if (n < 30) {
-                return say(decena) + "-i-" + say(unidad).toLowerCase();
+                return say(decena) + "-i-" + say(unitat).toLowerCase();
             }
-            return say(decena) + "-" + say(unidad).toLowerCase();
+            return say(decena) + "-" + say(unitat).toLowerCase();
         }
 
-
-        if (n >= 100) {
-            int centena = (int) (n / 100);
-            int resto = (int) (n % 100);
-            String nombreString = "";
-
-            if (centena == 1) {
-                nombreString = "Cent";
-            } else {
-                nombreString = say(centena) + "-cents";
-            }
-            String espai = "";
-            if (resto != 0) {
-                espai = " ";
-                nombreString += espai + say(resto).toLowerCase();
-            }
-            return nombreString;
+        if (n >= 100 && n < 1000) {
+            divisor = 100;
+            xifra = "Cent";
+            xifres = "-cents";
+            String nombre = calculaNombre(n, divisor, xifra, xifres);
+            return nombre;
+        }
+        if (n > 999) {
+            divisor = 1000;
+            xifra = "Mil";
+            xifres = " mil";
+            String nombre = calculaNombre(n, divisor, xifra, xifres);
+            return nombre;
         }
         return null;
     }
+
+    private static String calculaNombre(long n, int divisor, String xifra, String xifres) {
+        String nombre = "";
+        int centena = (int) (n / divisor);
+        int resto = (int) (n % divisor);
+        String espai = " ";
+
+        if (centena == 1) {
+            nombre = xifra;
+        } else {
+            nombre = say(centena) + xifres;
+        }
+        if (resto != 0) {
+            nombre += espai + say(resto).toLowerCase();
+        }
+        return nombre;
+    }
+
+
 }
--- src/NumbersCatTest.java
+++ src/NumbersCatTest.java
@@ -1,73 +1,90 @@
-import org.testng.annotations.Ignore;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.testng.AssertJUnit.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
 
 public class NumbersCatTest {
+    /*
+    1. Zero
+    2. Unitats
+    3. Negatius
+    4. Decenes
+        4.1 30 - 99
+        4.2 20 - 29
+        4.3 10 - 19
+     */
 
     @Test
-    public void donar_0_retorna_Zero() {
+    public void donat_0_retorna_Zero() {
         assertEquals("Zero", NumbersCat.say(0l));
     }
 
     @Test
-    public void donar_1_retorna_Un() {
+    public void donat_1_retorna_Un() {
         assertEquals("Un", NumbersCat.say(1l));
     }
 
     @Test
-    public void donar_9_retorna_Nou() {
+    public void donat_2_retorna_Dos(){
+        assertEquals("Dos", NumbersCat.say(2l));
+    }
+
+    @Test
+    public void donat_9_retorna_Nou(){
         assertEquals("Nou", NumbersCat.say(9l));
     }
 
     @Test
-    public void donar_Menys3_retorna_Tres() {
-        assertEquals("Menys tres", NumbersCat.say(-3l));
+    public void donat_menys_5_retorna_Menys_cinc(){
+        assertEquals("Menys cinc", NumbersCat.say(-5l));
     }
 
     @Test
-    public void donar_30_retorna_Trenta() {
+    public void donat_30_retorna_Trenta(){
         assertEquals("Trenta", NumbersCat.say(30l));
     }
 
     @Test
-    public void donat_39() {
-        assertEquals(("Trenta-nou"), NumbersCat.say(39));
+    public void donat_31_retorna_Trenta_un(){
+        assertEquals("Trenta-un", NumbersCat.say(31l));
     }
 
     @Test
-    public void donat41() {
-        assertEquals(("Quaranta-un"), NumbersCat.say(41l));
+    public void donat_41_retorna_Quaranta_un(){
+        assertEquals("Quaranta-un", NumbersCat.say(41l));
     }
 
     @Test
-    public void donat40() {
-        assertEquals(("Quaranta"), NumbersCat.say(40l));
+    public void donat_99_retorna_Noranta_nou(){
+        assertEquals("Noranta-nou", NumbersCat.say(99l));
     }
 
     @Test
-    public void donat21() {
-        assertEquals(("Vint-i-un"), NumbersCat.say(21l));
+    public void donat_20_retorna_Vint(){
+        assertEquals("Vint", NumbersCat.say(20l));
+    }
+    @Test
+    public void donat_21_retorna_Vint_i_un(){
+        assertEquals("Vint-i-un", NumbersCat.say(21l));
     }
 
     @Test
-    public void donat100() {
-        assertEquals(("Cent"), NumbersCat.say(100l));
+    public void donat_100_retorna_Cent(){
+        assertEquals("Cent", NumbersCat.say(100l));
     }
 
     @Test
-    public void donat200() {
-        assertEquals(("Dos-cents"), NumbersCat.say(200l));
+    public void donat_999_retorna_Nou_cents_noranta_nou(){
+        assertEquals("Nou-cents noranta-nou", NumbersCat.say(999l));
     }
 
     @Test
-    public void donat427() {
-        assertEquals(("Quatre-cents vint-i-set"), NumbersCat.say(427l));
+    public void donat_1000_retorna_Mil(){
+        assertEquals("Mil", NumbersCat.say(1000l));
     }
 
     @Test
-    public void donat999() {
-        assertEquals(("Nou-cents noranta-nou"), NumbersCat.say(999l));
+    public void donat_1057_retorna_Mil_cinquanta_set(){
+        assertEquals("Mil cinquanta-set", NumbersCat.say(1057l));
     }
 }
-
